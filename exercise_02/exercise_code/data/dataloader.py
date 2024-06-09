a25e0ac3e587f0f50dcc47882066757153285423
Muhammed Elmasry <61628083+Muhammed-Elmasry@users.noreply.github.com>
2024-06-09 15:58:18 +0200

    Add files via upload

---
+++ exercise_02/exercise_code/data/dataloader.py
@@ -0,0 +1,86 @@
+"""Definition of Dataloader"""
+
+import numpy as np
+
+
+class DataLoader:
+    """
+    Dataloader Class
+    Defines an iterable batch-sampler over a given dataset
+    """
+    def __init__(self, dataset, batch_size=1, shuffle=False, drop_last=False):
+        """
+        :param dataset: dataset from which to load the data
+        :param batch_size: how many samples per batch to load
+        :param shuffle: set to True to have the data reshuffled at every epoch
+        :param drop_last: set to True to drop the last incomplete batch,
+            if the dataset size is not divisible by the batch size.
+            If False and the size of dataset is not divisible by the batch
+            size, then the last batch will be smaller.
+        """
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.drop_last = drop_last
+
+    def __iter__(self):
+        ########################################################################
+        # TODO:                                                                #
+        # Define an iterable function that samples batches from the dataset.   #
+        # Each batch should be a dict containing numpy arrays of length        #
+        # batch_size (except for the last batch if drop_last=True)             #
+        # Hints:                                                               #
+        #   - np.random.permutation(n) can be used to get a list of all        #
+        #     numbers from 0 to n-1 in a random order                          #
+        #   - To load data efficiently, you should try to load only those      #
+        #     samples from the dataset that are needed for the current batch.  #
+        #     An easy way to do this is to build a generator with the yield    #
+        #     keyword, see https://wiki.python.org/moin/Generators             #
+        #   - Have a look at the "DataLoader" notebook first. This function is #
+        #     supposed to combine the functions:                               #
+        #       - combine_batch_dicts                                          #
+        #       - batch_to_numpy                                               #
+        #       - build_batch_iterator                                         #
+        #     in section 1 of the notebook.                                    #
+        ########################################################################
+        indices = np.arange(len(self.dataset))
+
+        if self.shuffle:
+            indices = np.random.permutation(indices)
+
+        num_batches = len(indices) // self.batch_size if self.drop_last else (len(indices) + self.batch_size - 1) // self.batch_size
+
+        for i in range(num_batches):
+            if i == num_batches - 1 and not self.drop_last:
+                batch_indices = indices[i * self.batch_size:]
+            else:
+                batch_indices = indices[i * self.batch_size: (i + 1) * self.batch_size]
+
+            # Create a batch dictionary to hold batch data
+            batch_dict = {}
+
+            for key in self.dataset[0].keys():  # Assuming all samples in the dataset have the same keys
+                batch_data = [self.dataset[idx][key] for idx in batch_indices]
+                batch_dict[key] = np.array(batch_data)
+
+            yield batch_dict
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+
+    def __len__(self):
+        length = None
+        ########################################################################
+        # TODO:                                                                #
+        # Return the length of the dataloader                                  #
+        # Hint: this is the number of batches you can sample from the dataset. #
+        # Don't forget to check for drop last!                                 #
+        ########################################################################
+        
+        if self.drop_last:
+            return len(self.dataset) // self.batch_size
+        else:
+            return (len(self.dataset) + self.batch_size - 1) // self.batch_size
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
